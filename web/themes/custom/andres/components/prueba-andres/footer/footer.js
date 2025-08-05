81f30bed266112cfa031848edb02a81d49ec9f99
Andres Alvarez <andres.alvarez@Andress-MacBook-Pro.local>
2025-08-04 22:08:21 -0500

    ajustes

---
+++ web/themes/custom/andres/components/prueba-andres/footer/footer.js
@@ -0,0 +1,8 @@
+/******/ (function() { // webpackBootstrap
+/*!****************************************************!*\
+  !*** ./components/prueba-andres/footer/_footer.js ***!
+  \****************************************************/
+console.log('Prueba Componentes loaded');
+/******/ })()
+;
+//# sourceMappingURL=footer.js.map
