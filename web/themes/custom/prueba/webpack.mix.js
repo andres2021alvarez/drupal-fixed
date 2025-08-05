292928b8da40f0089d74a24731427d8b438f2712
Andres Alvarez <andres.alvarez@Andress-MacBook-Pro.local>
2025-08-04 22:55:41 -0500

    prueba

--- web/themes/custom/prueba/webpack.mix.js
+++ web/themes/custom/prueba/webpack.mix.js
@@ -8,21 +8,39 @@
  |
 */
 /* eslint-disable */
-require('dotenv').config({ path: '.env.local' });
-const mix = require('laravel-mix');
-const glob = require('glob');
-require('laravel-mix-stylelint');
-require('laravel-mix-copy-watched');
+require("dotenv").config({ path: ".env.local" });
+const mix = require("laravel-mix");
+const glob = require("glob");
+const path = require("path");
+const { log } = require("console");
+require("laravel-mix-stylelint");
+require("laravel-mix-copy-watched");
 
 /*
-  |--------------------------------------------------------------------------
-  | Configuration
-  |--------------------------------------------------------------------------
+ |--------------------------------------------------------------------------
+ | Configuration
+ |--------------------------------------------------------------------------
 */
 mix
   .sourceMaps()
   .webpackConfig({
-    devtool: 'source-map',
+    resolve: {
+      symlinks: false, // Cambia esto a false en lugar de true
+      modules: [
+        path.resolve(__dirname, "node_modules"),
+        path.resolve(__dirname, "components"),
+        path.resolve(__dirname, "components/global"), // Asegúrate de incluir la ruta específica
+      ],
+    },
+    watchOptions: {
+      followSymlinks: true,
+      ignored: /node_modules/,
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+    snapshot: {
+      managedPaths: [],
+    },
   })
   .disableNotifications()
   .options({
@@ -30,68 +48,68 @@ mix
   });
 
 /*
-  |--------------------------------------------------------------------------
-  | Browsersync
-  |--------------------------------------------------------------------------
+ |--------------------------------------------------------------------------
+ | Browsersync
+ |--------------------------------------------------------------------------
 */
 mix.browserSync({
   proxy: process.env.DRUPAL_BASE_URL,
   files: [
-    'components/**/*.css',
-    'components/**/*.js',
-    'components/**/*.twig',
-    'templates/**/*.twig',
-    'build/css/*.css',
-    'build/js/*.js',
+    path.resolve(__dirname, "components/**/*.scss"),
+    path.resolve(__dirname, "components/**/*.js"),
+    path.resolve(__dirname, "components/**/*.twig"),
+    path.resolve(__dirname, "templates/**/*.twig"),
+    path.resolve(__dirname, "build/css/*.css"),
+    path.resolve(__dirname, "build/js/*.js"),
   ],
   stream: true,
 });
 
 /*
-  |--------------------------------------------------------------------------
-  | SASS
-  |--------------------------------------------------------------------------
+ |--------------------------------------------------------------------------
+ | SASS
+ |--------------------------------------------------------------------------
 */
-mix.sass('src/scss/main.style.scss', 'build/css/main.style.css');
+mix.sass("src/scss/main.style.scss", "build/css/main.style.css");
 
-for (const sourcePath of glob.sync('components/**/*.scss')) {
+for (const sourcePath of glob.sync('components/**/*.scss', { follow: true })) {
   const destinationPath = sourcePath.replace(/\.scss$/, '.css');
   mix.sass(sourcePath, destinationPath);
 }
 
 /*
-  |--------------------------------------------------------------------------
-  | JS
-  |--------------------------------------------------------------------------
+ |--------------------------------------------------------------------------
+ | JS
+ |--------------------------------------------------------------------------
 */
-mix.js('src/js/main.script.js', 'build/js/main.script.js');
+mix.js("src/js/main.script.js", "build/js/main.script.js");
 
-for (const sourcePath of glob.sync('components/**/_*.js')) {
-  const destinationPath = sourcePath.replace(/\/_([^/]+\.js)$/, '/$1');
+for (const sourcePath of glob.sync("components/**/_*.js")) {
+  const destinationPath = sourcePath.replace(/\/_([^/]+\.js)$/, "/$1");
   mix.js(sourcePath, destinationPath);
 }
 
 /*
-  |--------------------------------------------------------------------------
-  | Style Lint
-  |--------------------------------------------------------------------------
+ |--------------------------------------------------------------------------
+ | Style Lint
+ |--------------------------------------------------------------------------
 */
 mix.stylelint({
-  configFile: './.stylelintrc.json',
-  context: './src',
+  configFile: "./.stylelintrc.json",
+  context: "./src",
   failOnError: false,
-  files: ['**/*.scss'],
+  files: ["**/*.scss"],
   quiet: false,
-  customSyntax: 'postcss-scss',
+  customSyntax: "postcss-scss",
 });
 
 /*
-  |--------------------------------------------------------------------------
-  * IMAGES / ICONS / VIDEOS / FONTS
-  |--------------------------------------------------------------------------
-  */
-// * Directly copies the images, icons and fonts with no optimizations on the images
-mix.copyDirectoryWatched('src/assets/images', 'build/assets/images');
-mix.copyDirectoryWatched('src/assets/icons', 'build/assets/icons');
-mix.copyDirectoryWatched('src/assets/videos', 'build/assets/videos');
-mix.copyDirectoryWatched('src/assets/fonts/**/*', 'build/fonts');
+ |--------------------------------------------------------------------------
+ | IMAGES / ICONS / VIDEOS / FONTS
+ |--------------------------------------------------------------------------
+*/
+// Copia los recursos estáticos sin optimización de imágenes
+mix.copyDirectoryWatched("src/assets/images", "build/assets/images");
+mix.copyDirectoryWatched("src/assets/icons", "build/assets/icons");
+mix.copyDirectoryWatched("src/assets/videos", "build/assets/videos");
+mix.copyDirectoryWatched("src/assets/fonts/**/*", "build/fonts");
